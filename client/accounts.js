d7360af888f56c86b5c80e291d3ecdf19558c7f8
coreyaus <corey@freshhold.com.co>
2015-01-26 00:47:42 +1000

    get accounts and login working. Fixes #3

---
+++ client/accounts.js
@@ -0,0 +1,14 @@
+Meteor.startup(function () {
+  AccountsEntry.config({
+    homeRoute: '/',
+    dashboardRoute: '/',
+    profileRoute: '/profile/edit',
+    passwordSignupFields: 'USERNAME_AND_EMAIL',
+    extraSignUpFields: [{
+      field: 'name',
+      label: 'Full Name',
+      type: 'text',
+      required: true
+    }]
+  })
+})
